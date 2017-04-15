9fd424f6419a6d25468038100d74af5af4b56364
Erik Campobadal <ConsoleTVs@gmail.com>
2017-04-15 14:25:19 +0200

    Update layout.blade.php

--- src/Views/scripts/layout.blade.php
+++ src/Views/scripts/layout.blade.php
@@ -1,4 +1,4 @@
-<script src="https://gitcdn.xyz/cdn/Laralum/Advertisements/7174d5940d4789431b9e27227999bc449a815eb1/src/Assets/ads.js" type="text/javascript"></script>
+<script src="https://gitcdn.xyz/cdn/Laralum/Advertisements/5736ba51e1f1b24d57388140cb7d1f5339f0d069/src/Assets/ads.js" type="text/javascript"></script>
 <script src="https://gitcdn.xyz/cdn/vincepare/iframeTracker-jquery/56960ccf4bc600754348832e7e5fdc092e562d35/jquery.iframetracker.js" type="text/javascript"></script>
 @yield('blocker-code')
 <script>
