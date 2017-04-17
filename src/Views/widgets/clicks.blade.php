13e363c9e1fbbf77e67aeacae54d9f51f1931906
24aitor <contact@aitorriba.com>
2017-04-17 03:32:35 +0200

    Widget Translations

--- src/Views/widgets/clicks.blade.php
+++ src/Views/widgets/clicks.blade.php
@@ -1,7 +1,7 @@
 {!!
     ConsoleTVs\Charts\Facades\Charts::multiDatabase('line', 'highcharts')
-            ->title('Advertisements clicks on the last 7 days')->dimensions(0, 400)->responsive(false)
-            ->dataset('Total Clicks', \Laralum\Advertisements\Models\View::where('click', 1)->get())
-            ->dataset('Unique Clicks', \Laralum\Advertisements\Models\View::where('click', 1)->get()->unique('ip'))
+            ->title(__('laralum_advertisements::general.ads_clicks_7_days'))->dimensions(0, 400)->responsive(false)
+            ->dataset(__('laralum_advertisements::general.total_clicks'), \Laralum\Advertisements\Models\View::where('click', 1)->get())
+            ->dataset(__('laralum_advertisements::general.total_u_clicks'), \Laralum\Advertisements\Models\View::where('click', 1)->get()->unique('ip'))
             ->lastByDay(7, true)->elementLabel('Views')->render();
 !!}
--- src/Views/widgets/views.blade.php
+++ src/Views/widgets/views.blade.php
@@ -1,7 +1,7 @@
 {!!
     ConsoleTVs\Charts\Facades\Charts::multiDatabase('line', 'highcharts')
-            ->title('Advertisements views on the last 7 days')->dimensions(0, 400)->responsive(false)
-            ->dataset('Total Views', \Laralum\Advertisements\Models\View::where('click', 0)->get())
-            ->dataset('Unique Views', \Laralum\Advertisements\Models\View::where('click', 0)->get()->unique('ip'))
+            ->title(__('laralum_advertisements::general.ads_views_7_days'))->dimensions(0, 400)->responsive(false)
+            ->dataset(__('laralum_advertisements::general.total_views'), \Laralum\Advertisements\Models\View::where('click', 0)->get())
+            ->dataset(__('laralum_advertisements::general.total_u_views'), \Laralum\Advertisements\Models\View::where('click', 0)->get()->unique('ip'))
             ->lastByDay(7, true)->elementLabel('Views')->render();
 !!}
